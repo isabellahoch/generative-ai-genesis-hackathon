2a9161279c9bf57abd449a221e297bac9294341d
isabellahoch <40366749+isabellahoch@users.noreply.github.com>
2023-06-18 21:57:30 -0400

    feat: add API capacity to generate a specific artist as well

--- backend/app.py
+++ backend/app.py
@@ -23,8 +23,10 @@ CORS(app)
 
 artists = ["Claude Monet", "Auguste Renoir", "Edgar Degas", "Berthe Morisot", "Edouard Manet", "Camille Pissarro", "Mary Cassatt", "Georges Seurat"] # many of the primary impressionist artists + added in Seurat (pointillism) as well
 
-def generate_full_prompt(entry):
-  artist = random.choice(artists)
+def get_random_artist():
+    return random.choice(artists)
+
+def generate_full_prompt(entry, artist):
   return entry+" in the style of a "+artist+" painting"
 
 def huggingface_query(payload, headers, url):
@@ -32,10 +34,11 @@ def huggingface_query(payload, headers, url):
 	return response.content
 
 @app.route('/', methods=['GET'])
-def test():
+def main_endpoint():
     args = request.args
     query = args.get("q")
-    prompt = generate_full_prompt(query)
+    artist = get_random_artist()
+    prompt = generate_full_prompt(query, artist)
     if query is None or query == '':
         query='question mark on the beach' # file not found / empty query failsafe
     if torch.cuda.is_available():
@@ -52,5 +55,32 @@ def test():
         if(image_bytes is None): 
             return None
         return base64.b64encode(image_bytes)
+
+@app.route('/<artist_name>', methods=['GET'])
+def create(artist_name=None):
+    args = request.args
+    query = args.get("q")
+    if(artist_name is not None):
+        artist = artist_name
+    else:
+        artist = get_random_artist()
+    prompt = generate_full_prompt(query, artist)
+    if query is None or query == '':
+        query='question mark on the beach' # file not found / empty query failsafe
+    if torch.cuda.is_available():
+        pipeline = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+        pipe = pipeline.to('cuda')
+        torch.cuda.empty_cache()
+        image = pipe(prompt).images[0]
+        return image
+    else:
+        headers = {"Authorization": "Bearer "+os.environ.get("BEARER_TOKEN")}
+        image_bytes = huggingface_query({
+            "inputs": prompt,
+        }, headers, "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5")
+        if(image_bytes is None): 
+            return None
+        return base64.b64encode(image_bytes)
+
 
 app.run(host='0.0.0.0')
